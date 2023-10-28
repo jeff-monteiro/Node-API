3d887899061b99096e88f6e909cb2443b03a7caa
Jefferson <jeffmonteiro61@icloud.com>
2023-10-27 22:39:58 -0300

    testing:commmit_with_a_new_key

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ const users = require('./controllers/users')
 app.use('/', users)
 
 app.listen(8080, () => {
-    console.log("Server is running on port 8080")
+    console.log("Server now is running on port 8080")
 })
